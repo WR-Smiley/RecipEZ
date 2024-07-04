b79a56146bc9e37dc44e4e5a1859b3f0f22742e0
WR Smiley <smileywestin@gmail.com>
2024-07-04 11:44:42 -0600

    Creates layout shells for adding/editing recipes and ingredients.

---
+++ app/src/main/java/com/smileydev/recipez/activities/AddIngredient.java
@@ -0,0 +1,13 @@
+package com.smileydev.recipez.activities;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class AddIngredient extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
