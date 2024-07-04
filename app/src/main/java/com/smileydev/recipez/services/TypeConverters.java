507bd89a4e1c0402111ebe5032e5985c48548b5d
WR Smiley <smileywestin@gmail.com>
2024-07-04 09:32:16 -0600

    Creates IngredientAdapter. Sets up type converters for ArrayLists and Dates.

--- app/src/main/java/com/smileydev/recipez/services/TypeConverters.java
+++ app/src/main/java/com/smileydev/recipez/services/TypeConverters.java
@@ -1,4 +1,42 @@
 package com.smileydev.recipez.services;
 
+import androidx.room.TypeConverter;
+
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+
+import java.lang.reflect.Type;
+import java.util.ArrayList;
+import java.util.Date;
+
 public class TypeConverters {
+
+    @TypeConverter
+    public static String fromArrayList(ArrayList<String> list) {
+        if (list == null) {
+            return null;
+        }
+        Gson gson = new Gson();
+        return gson.toJson(list);
+    }
+
+    @TypeConverter
+    public static ArrayList<String> fromString(String value) {
+        if  (value == null) {
+            return null;
+        }
+        Gson gson = new Gson();
+        Type listType = new TypeToken<ArrayList<String>>() {}.getType();
+        return gson.fromJson(value, listType);
+    }
+
+    @TypeConverter
+    public static Date fromTimestamp(Long value) {
+        return value == null ? null : new Date(value);
+    }
+
+    @TypeConverter
+    public static Long fromDate(Date date) {
+        return date == null ? null : date.getTime();
+    }
 }
