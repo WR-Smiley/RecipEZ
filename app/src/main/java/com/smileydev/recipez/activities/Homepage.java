99752138e237de0bc18f7efcec571f96e964e0fa
WR Smiley <smileywestin@gmail.com>
2024-07-06 15:43:33 -0600

    Designs activities for adding and editing recipes and ingredients.

--- app/src/main/java/com/smileydev/recipez/activities/Homepage.java
+++ app/src/main/java/com/smileydev/recipez/activities/Homepage.java
@@ -1,6 +1,9 @@
 package com.smileydev.recipez.activities;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -16,6 +19,17 @@ public class Homepage extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_home);
+        Button newRecipe = findViewById(R.id.addRecipe);
+
+        newRecipe.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent addRecipe = new Intent(Homepage.this, AddRecipe.class);
+                startActivity(addRecipe);
+            }
+        });
+
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
