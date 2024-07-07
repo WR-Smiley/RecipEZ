490ae70fb5d156fecddc2f8107e46c91ba70c1f6
WR Smiley <smileywestin@gmail.com>
2024-07-06 19:25:23 -0600

    Add buttons to each layout for database interaction. Adds activity and layout for just viewing recipe.

--- app/src/main/java/com/smileydev/recipez/activities/AddRecipe.java
+++ app/src/main/java/com/smileydev/recipez/activities/AddRecipe.java
@@ -9,37 +9,16 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 import com.smileydev.recipez.R;
-import com.smileydev.recipez.entities.Ingredient;
-import com.smileydev.recipez.entities.User;
-
-import java.util.ArrayList;
-import java.util.Date;
 
 public class AddRecipe extends AppCompatActivity {
-
-    private int recipeId;
-
-    private String name;
-
-    private Date dateCreated;
-
-    private Date lastUpdate;
-
-    private int pplServed;
-
-    private int timeEstimate;
-
-    private User createdBy;
-
-    private ArrayList<String> instructions;
-
-    private ArrayList<Ingredient> ingredients;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_add_recipe);
+
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
